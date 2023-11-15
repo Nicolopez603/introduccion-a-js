fc0915c9d18f7d67182eb4105c566fbd755b6b1b
Nicolas Lopez <81532585+Nicolopez603@users.noreply.github.com>
2023-11-14 21:19:56 -0300

    complete the first task
    complete the first task adding a text-box with all the information of the user

--- tareas/clase-5/tarea1.js
+++ tareas/clase-5/tarea1.js
@@ -1,12 +1,31 @@
-document.querySelector('#calcular-salario-mensual').onclick = function(){
-    const salarioAnual = Number(document.querySelector('#salario-anual').value);
-    const salarioMensual = calcularSalarioMensual(salarioAnual);
+//TAREA2: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
+// creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
+// también vamos a crear un <h1> que diga Bienvenido!
+// vas a crear un botón de acción que una vez que lo apretás, va a
+// mostrar toda la información junta en un campo de texto
+// Y va a cambiar el <h1> para decir "Bienvenido, nombreDeUsuario"!
 
-    document.querySelector('#salario-mensual').value = salarioMensual;
+function showInformation() {
+    let $firstName = document.querySelector('#first-name').value
+    let $secondName = document.querySelector('#second-name').value
+    let $lastName = document.querySelector('#last-name').value
+    let $ageUser = document.querySelector('#age-user').value
+    let $messageWelcome = `Welcome ${$firstName} ${$secondName}!`
 
-    return false;
+    document.querySelector('#welcome').textContent = $messageWelcome
+
+    let informationComplete =
+        'First name: ' +
+        $firstName +
+        '\nSecond name: ' +
+        $secondName +
+        '\nLast Name: ' +
+        $lastName +
+        '\nAge: ' +
+        $ageUser
+
+    document.querySelector('#fieldText').value = informationComplete
 }
 
-function calcularSalarioMensual(salarioAnual){
-    return salarioAnual / 12;
-}
+
+
